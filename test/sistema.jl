7b5b15b6986e5b002770fb34efe2686088542db9
PatrizioCaddeo <patrizio.caddeo@hotmail.it>
2017-04-06 22:08:53 +0200

    sistema aggiustato

--- test/sistema.jl
+++ test/sistema.jl
@@ -1,69 +1,36 @@
-
-using PyPlot
 function sistema(x0,y0,w,N) # x0 ed y0 sono rispettivamente posizione e velocità iniziali, w è la pulsazione ed N è il grado di precisione
 
-a=Array{Float64}(N+1) # Vettore di appoggio per la posizione x
-b=Array{Float64}(N+1) # Vettore di appoggio per la posizione y
+	a=Array{Float64}(N+1) # Vettore di appoggio per la posizione x
+	b=Array{Float64}(N+1) # Vettore di appoggio per la posizione y
 
-a[1]=x0
-b[1]=y0
-T=2*pi/w       # T è il periodo delle soluzioni
-dt=T/N # dt è l'intervallo di tempo tra due istanti successivi nella nostra approssimazione
+	a[1]=x0
+	b[1]=y0
+	T=2*pi/w       # T è il periodo delle soluzioni
+	dt=T/N         # dt è l'intervallo di tempo tra due istanti successivi nella nostra approssimazione
 
-for k in 1:N
-	a[k+1]=a[k]+b[k]*dt       # Formula di Taylor: x(t+dt)=x(t)+x'(t)dt=x(t)+y(t)dt
-	b[k+1]=b[k]-w^2*a[k]*dt   # Formula di Taylor: y(t+dt)=y(t)+y'(t)dt=x(t)-(w^2)x(t)dt
-end
+	for k in 1:N
+		a[k+1]=a[k]+b[k]*dt       # Formula di Taylor: x(t+dt)=x(t)+x'(t)dt=x(t)+y(t)dt
+		b[k+1]=b[k]-w^2*a[k]*dt   # Formula di Taylor: y(t+dt)=y(t)+y'(t)dt=x(t)-(w^2)x(t)dt
+	end
 
 # Le funzioni di ritorno saranno interpolazioni lineari basate sui vettori di appoggio:
-function x(t) 
-	t_modulo = FLoat64 # t_modulo è il valore di t riscalato
-	if(t>=0)
-		t_modulo = t % T
-	else
-		t_modulo = (t % T) + T
-	end
-	n = floor(Int64, t/(T/N) ) # n è l'indice di approssimazione
-	if floor n==0
-		return a[1]
-	elseif n==N
-		return a[N+1]
-	else
-		return a[n+1]+(a[n+2]-a[n+1])/(T/N)*(t-(n+1)T/N)
-	end
-end
+	function x(t) 
+		t_modulo = Float64 # t_modulo sarà il valore di t riscalato nell'intervallo [0,T]
+		if(t>=0)
+			t_modulo = t % T
+		else
+			t_modulo = (t % T) + T
+		end
+		n = floor(Int64, t_modulo/(T/N) ) # n è l'indice di approssimazione
+		if n==0
+			return a[1]
+		elseif n==N
+			return a[N+1]
+		else
+			return a[n+1]+(a[n+2]-a[n+1])/(T/N)*(t_modulo-(n+1)T/N)
+		end
+	end	
 
-return x
-
-end
-
-
-
-function sistemaPar(s,v,w,N)
-
-a=Array{Float64}(N+1)
-b=Array{Float64}(N+1)
-
-a[1]=s
-b[1]=v
-
-@spawn for k in 1:N
-a[k+1]=a[k]+b[k]*2*pi/(N*w)
-b[k+1]=b[k]-w^2*a[k]*2*pi/(N*w)
-end
-
-function x(t)
-if floor(Int64, t*N*w/(2*pi))==0
-return a[1]
-elseif floor(Int64, t*N*w/(2*pi))==N
-return a[N+1]
-else
-n=floor(Int64, t*N*w/(2*pi))
-return a[n+1]+(a[n+2]-a[n+1])/(2*pi/(N*w))*(t-2*(n+1)*pi/(N*w))
-end
-end
-
-return x
+	return x
 
 end
-
